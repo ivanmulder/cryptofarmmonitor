6a65a6262d4ba1b725358d2df4e910aadb9af0db
Marian Skalsky <marianskalsky@MacBook-Pro-uzivatela-Marian.local>
2018-04-27 15:40:28 +0200

    Initial implementation.

---
+++ json/sensor.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Sensor {
+    constructor(id, temperature) {
+        this.id = id;
+        this.temperature = temperature;
+    }
+}
+
+module.exports = Sensor;
---
+++ repo/sensorRepository.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Sensor = require('../json/sensor');
+const OwfsClient = require('owfs').Client;
+var owfs = new OwfsClient('127.0.0.1', 4304);
+
+class SensorRepository {
+    constructor() {
+        this.sensors = new Map([
+            [1, new Sensor(1, 24)],
+            [2, new Sensor(2, 25)],
+            [3, new Sensor(3, 26)],
+            [4, new Sensor(4, 27)]
+        ]);
+    }
+
+    getById(id) {
+        return this.sensors.get(id);
+    }
+
+    getAll() {
+        try{
+            owfs.dirallslash("/", function(err, directories){ 
+                                    directories.forEach(function(element) {
+                                    console.log('Element: ' + element);
+                                    })
+            })
+        }
+        catch(err) {
+            console.log("owfs error", err);
+        }
+        return Array.from(this.sensors.values());
+    }
+
+    // remove() {
+    //     const keys = Array.from(this.sensors.keys());
+    //     this.sensors.delete(keys[keys.length - 1]);
+    // }
+
+    // save(sensor) {
+    //     if (this.getById(sensor.id) !== undefined) {
+    //         this.sensors[sensor.id] = sensor;
+    //         return 'Updated Sensor with id=' + sensor.id;
+    //     }
+    //     else {
+    //         this.sensors.set(sensor.id, sensor);
+    //         return 'Added Sensor with id=' + sensor.id;
+    //     }
+    // }
+}
+
+const sensorRepository = new SensorRepository();
+
+module.exports = sensorRepository;
